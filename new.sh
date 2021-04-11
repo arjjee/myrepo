049f09377944bfbddbb0bb4c640b81d2160f2a28
Raju Singh <arrjee@gmail.com>
2021-04-11 12:09:00 +0100

    new update to code scripts

--- new.sh
+++ new.sh
@@ -3,4 +3,4 @@ export GIT_COMMITTER_DATE="Wed Apr 11 12:01 2020 +0100"
 export GIT_AUTHOR_DATE="Wed Apr 11 12:09 2021 +0100"
 git add .
 git commit -am " new update to code scripts"
-git push origin master
+git push --set-upstream origin master
